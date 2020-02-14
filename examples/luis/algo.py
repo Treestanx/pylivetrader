c2abcd9872b9d8cf5248a83ad0284a167214bb7f
Treestanx <tmarinoman@gmail.com>
2020-02-14 12:12:01 -0500

    Update algo.py

--- examples/luis/algo.py
+++ examples/luis/algo.py
@@ -2,28 +2,30 @@ from pylivetrader.api import order_target, record, symbol
 
 
 def initialize(context):
-    context.i = 0
-    context.asset = symbol('AAPL')
+    """
+    Called once at the start of the algorithm.
+    """   
+    
+    # Here are any algorithm 'constants' we'll be using
+    context.target_leverage = 1.0
+    
+    # Here are the ETFs we want to trade along with the weights 
+    # Ensure they add to 1.00
+    context.etfs = {
+        symbol('TYD'): 0.1, # Daily 7-10 Year Treasury Bull 3X Shares
+        symbol('TMF'): 0.2, # Daily 20+ Year Treasury Bull 3X Shares
+        symbol('EDZ'): 0.2, # Daily MSCI Emerging Markets Bear 3X Shares
+        symbol('SPXL'): 0.5, # Daily S&P 500 Bull 3X Shares
+
+    }
+    
+    # Set commision model for Robinhood
+    set_commission(commission.PerShare(cost=0.0, min_trade_cost=0.0))
+ 
+    # Rebalance our portfolio to maintain target weights
+    schedule_function(rebalance, date_rules.every_day(), time_rules.market_open(minutes = 35))
 
 
-def handle_data(context, data):
-    # Compute averages
-    # data.history() has to be called with the same params
-    # from above and returns a pandas dataframe.
-    short_mavg = data.history(
-        context.asset, 'price', bar_count=100, frequency="1m").mean()
-    long_mavg = data.history(
-        context.asset, 'price', bar_count=300, frequency="1m").mean()
-
-    # Trading logic
-    if short_mavg > long_mavg:
-        # order_target orders as many shares as needed to
-        # achieve the desired number of shares.
-        order_target(context.asset, 100)
-    elif short_mavg < long_mavg:
-        order_target(context.asset, 0)
-
-    # Save values for later inspection
-    record(AAPL=data.current(context.asset, 'price'),
-           short_mavg=short_mavg,
-           long_mavg=long_mavg)
+def rebalance(context, data):
+    for stock, weight in context.etfs.items():
+        order_target_percent(stock, weight*context.target_leverage)
